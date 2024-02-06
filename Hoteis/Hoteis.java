48e592ae6498d926bb5cd0247156be6fc46fef8a
Enzo Faria <143216954+Enzofdl@users.noreply.github.com>
2024-02-06 11:15:49 -0300

    Atualizar o Hoteis.java

--- Hoteis/Hoteis.java
+++ Hoteis/Hoteis.java
@@ -1,1 +1,34 @@
-public Hoteis{}
+package Hoteis
+
+public class Hoteis{
+private final String cnpj;
+private final String nome;
+private String nomefantasia;
+private String endereco;
+private String anocriacao;
+private float avaliacao;
+private boolean pets;
+private int numeroquartos;
+private Quartos[] locacao;
+private String descricao;
+private String mensagem;
+private String cidade;
+private int[][] tipoquarto;
+private float desconto;
+
+public Hoteis(String cnpj, String nome, String nomefantasia, String endereco, String cidade, 
+String anocriacao, float avaliacao, boolean pets, int numeroquartos){
+
+
+}
+
+
+
+
+
+
+
+
+
+
+}
