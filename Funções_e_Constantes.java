9062a286da856010f0f6b994a2135f31db366b72
Enzo Faria <143216954+Enzofdl@users.noreply.github.com>
2024-02-15 11:37:58 -0300

    Create Funções_e_Constantes.java

---
+++ Funções_e_Constantes.java
@@ -0,0 +1,19 @@
+public class Funções_e_Constantes {
+
+
+
+    public static boolean validacnpj(){return true;}
+    public static boolean validacpf(){return true;}
+    public static boolean validadata(){return true;}
+    public static boolean validahorario(){return true;}
+    public static boolean validaendereco(){return true;}
+    public static boolean validaquarto(){return true;}
+    public static boolean validaprecos(){return true;}
+    public static boolean validasalario(){return true;}
+    public static boolean validavoo(){return true;}
+
+
+
+
+
+}
