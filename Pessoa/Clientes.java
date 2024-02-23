aa4d8f77c013b21d6b8ef1b294f0f23471a85eb4
Caio <caio.correa@ufu.br>
2024-02-23 20:25:12 -0300

    .

--- Pessoa/Clientes.java
+++ Pessoa/Clientes.java
@@ -2,7 +2,7 @@ package Pessoa;
 
 public class Clientes extends Pessoa {
     private String data_cadastro, email;
-
+    private boolean VIP;
     public Clientes(String nome, String CPF, Data_nv data_nasc, Endereco end, String data_cadastro, String email)
     {
         super(nome, CPF, end,data_nasc);
@@ -10,7 +10,27 @@ public class Clientes extends Pessoa {
         this.email = email;
     }
 
+    public String getEmail() {
+        return email;
+    }
 
+    public boolean isVIP() {
+        return VIP;
+    }
 
+    public String getData_cadastro() {
+        return data_cadastro;
+    }
 
+    public void setData_cadastro(String data_cadastro) {
+        this.data_cadastro = data_cadastro;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setVIP(boolean VIP) {
+        this.VIP = VIP;
+    }
 }
