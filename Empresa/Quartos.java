d19afc94bd217d129a0642def464736bc0d85081
B-Tibaldi <159039607+B-Tibaldi@users.noreply.github.com>
2024-03-01 13:12:53 -0300

    Quartos.java

--- Empresa/Quartos.java
+++ Empresa/Quartos.java
@@ -1,21 +1,101 @@
 package Empresa;
 
-class Quartos extends Hoteis {
+public class Quartos {
 
-    public int disponiveis;
-    public boolean cancelamento;
-    public boolean desconto;
-    public int capacidade;
-    public float preco;
+  public boolean disponivel;
+  public int capacidade;
+  public Tempo checkIn;
+  public Tempo checkOut;
+  public boolean cancelamento;
+  public float desconto;
+  public float preco;
+  public boolean luxo;
 
+  public Quartos(boolean disponivel, int capacidade, boolean cancelamento, float preco, boolean luxo) {
+    this.disponivel = disponivel;
+    this.capacidade = capacidade;
+    this.cancelamento = cancelamento;
+    this.preco = preco;
+    this.luxo = luxo;
+  }
 
-    public Quartos (int disponiveisAux, boolean cancelamentoAux, boolean descontoAux, int capacidadeAux, float precoAux, String nome, String cnpj, String nomefantasia, String endereco, String anodecriacao, String horariocIn, String horariocOut, String tipoQuarto, float estrelasAux, boolean aceitaPetsAux, int nroQuartosAux) {
-        super(nome, cnpj, nomefantasia, endereco, anodecriacao, horariocIn, horariocOut, tipoQuarto, estrelasAux, aceitaPetsAux, nroQuartosAux);
-        disponiveis = disponiveisAux;
-        cancelamento = cancelamentoAux;
-        desconto = descontoAux;
-        capacidade = capacidadeAux;
-        preco = precoAux;
-    }
-    public Quartos(){}
+  public Quartos(boolean disponivel, int capacidade, boolean cancelamento, float preco, boolean luxo, Tempo checkIn,
+      Tempo checkOut) {
+    this(disponivel, capacidade, cancelamento, preco, luxo);
+    this.checkIn = checkIn;
+    this.checkOut = checkOut;
+  }
+
+  public boolean getDisponivel() {
+    return disponivel;
+  }
+
+  public void setDisponivel(boolean disponivel) {
+    this.disponivel = disponivel;
+  }
+
+  public int getCapacidade() {
+    return capacidade;
+  }
+
+  public void setCapacidade(int capacidade) {
+    this.capacidade = capacidade;
+  }
+
+  public Tempo getCheckOut() {
+    return checkOut;
+  }
+
+  public void setCheckOut(Tempo checkOut) {
+    if(checkOut.getHora() > checkIn.getHora() || (checkOut.getHora() == checkIn.getHora() && checkOut.getMin() > checkIn.getMin())) this.checkOut = checkOut; 
+    //Não permite Check Out antes do Check In
+  }
+
+  public Tempo getCheckIn() {
+    return checkIn;
+  }
+
+  public void setCheckIn(Tempo checkIn) {
+    this.checkIn = checkIn;
+  }
+
+  public boolean getCancelamento() {
+    return cancelamento;
+  }
+
+  public void setCancelamento(boolean cancelamento) {
+    this.cancelamento = cancelamento;
+  }
+
+  public float getPreco() {
+    return preco;
+  }
+
+  public void setPreco(float preco) {
+    if (isVip)
+      this.preco = aplicaDesconto(preco, desconto);
+    else
+      this.preco = preco;
+  }
+
+  public boolean getLuxo() {
+    return luxo;
+  }
+
+  public void setLuxo(boolean luxo) {
+    this.luxo = luxo;
+  }
+
+  public float getDesconto() {
+    return desconto;
+  }
+
+  public void setDesconto(float desconto) {
+    this.desconto = desconto;
+  }
+
+  public float aplicaDesconto(float preco, float desconto) {
+    return (preco - desconto);
+  }
+
 }
