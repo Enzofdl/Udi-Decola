62af4fbc1739972fbc13ad8f05ad2570d536d359
B-Tibaldi <159039607+B-Tibaldi@users.noreply.github.com>
2024-03-01 13:14:01 -0300

    Tempo.java

---
+++ Empresa/Tempo.java
@@ -0,0 +1,26 @@
+public class Tempo {
+  private int hora;
+  private int min;
+
+  public Tempo(int hora, int min) {
+    this.hora = hora;
+    this.min = min;
+  }
+
+  public int getHora(){
+    return hora;
+  }
+  public void setHora(int hora) {
+    if (hora >= 0 && hora <= 23) this.hora = hora;
+    else this.hora = 0;
+  }
+
+  public int getMin(){
+    return min;
+  }
+
+  public void setMin(int min) {
+    if(min >= 0 && min <= 59) this.min = min;
+    else this.min = 0;
+  }
+}
