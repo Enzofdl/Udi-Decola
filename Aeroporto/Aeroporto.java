1cbede786f385410cf30488be1b5825a7f3fa0e4
Enzo Faria <143216954+Enzofdl@users.noreply.github.com>
2024-03-06 15:05:25 -0300

    Update Aeroporto.java

--- Aeroporto/Aeroporto.java
+++ Aeroporto/Aeroporto.java
@@ -5,27 +5,14 @@ import Trecho.Trechos;
 
 public class Aeroporto {
   private String nome;
-  private String cidade;
-  private Trechos novoTrecho;
-  private int pos;
+  private final String cidade;
 
   ArrayList<Trechos> trechos = new ArrayList<Trechos>();
 
   public Aeroporto(String nome, String cidade, ArrayList<Trechos> trechos) {
     this.nome = nome;
     this.cidade = cidade;
-    this.trechos = trechos;
-  }
-  
-  public Aeroporto(String nome, String cidade, Aeroporto origem, Aeroporto destino, String nomeTrecho) {
-    this(nome, cidade);
-    this.trechos = new Trechos(origem, destino, nomeTrecho);
-  }
 
-  public Aeroporto(String nome, String cidade, ArrayList<Trechos> trechos, Trechos novoTrecho, int pos) {
-    this(nome, cidade, trechos);
-    this.novoTrecho = novoTrecho;
-    this.pos = pos;
   }
 
   public String getNome() {
@@ -36,61 +23,34 @@ public class Aeroporto {
     return cidade;
   }
 
-  public Trechos getNovoTrecho() {
-    return novoTrecho;
-  }
-
-  public int getpos() {
-    return pos;
-  }
-
   public void setNome(String nome) {
     this.nome = nome;
   }
 
-  public void setCidade(String cidade) {
-    this.cidade = cidade;
-  }
-
-  public void setNovoTrecho(Trechos novoTrecho) {
-    this.novoTrecho = novoTrecho;
-  }
-
-  public void setpos(int pos) {
-    this.pos = pos;
-  }
-
-  public ArrayList<Trechos> registraTrecho(ArrayList<Trechos> trechos, Trechos novoTrecho) {
+  public void registraTrecho(Trechos a) {
     trechos.add(novoTrecho);
-    return trechos;
   }
 
-  public ArrayList<Trechos> removeTrecho(ArrayList<Trechos> trechos, int pos) {
-    trechos.remove(pos);
-    return trechos;
+  public void removeTrecho(Trechos a) {
+    trechos.remove(a);
   }
 
-  public ArrayList<Trechos> alteraTrecho(ArrayList<Trechos> trechos, int pos, Trechos novoTrecho) {
-    trechos.set(pos, novoTrecho);
-    return trechos;
+  public void alteraTrecho(Trechos antigoTrecho, Trechos novoTrecho) {
+    trechos.remove(antigoTrecho);
+    trechos.add(novoTrecho);
   }
 
   
-  public void buscarTrecho(int pos) {
-    if(trechos.contains(pos)) {
-        Trechos buscado = trechos.get(pos);
-        String buscadoEscrito = buscado.toString();
-        System.out.println(buscadoEscrito);
-      }
-  }
+  public boolean BuscarTrecho(Trechos a){
+            return T.contains(a);
+        }
+  public Trechos BuscarTrecho(int a){
+            return T.get(a);
+        }
    
 
-  public void mostraTrechos() {
-    if (!trechos.isEmpty()) {
-      String trechoEscrito = trechos.toString();
-      System.out.println(trechoEscrito);
-    } else
-      System.out.println("Nao ha trechos de voo neste aeroporto!");
+  public ArrayList <Trechos> mostraTrechos() {
+    return trechos;
   }
 
 }
