accfbb0cd70a3392381b7793e40ec1de35525f21
Marcos Paulo Gomes <128519024+Marcospgp26@users.noreply.github.com>
2024-04-09 20:27:01 -0300

    DadosAeroporto.java DadosTrecho.java

--- Aeroporto/DadosAeroporto.java
+++ Aeroporto/DadosAeroporto.java
@@ -4,8 +4,49 @@ import java.util.ArrayList;
 public class DadosAeroporto {
     private static ArrayList<Aeroporto> vetaero = new ArrayList<Aeroporto>();
 
-    public static void cadastrar(Aeroporto a)
+    public static boolean cadastrar(Aeroporto a)
     {
+        if(Existe(a.getNome())) return false;
         vetaero.add(a);
+        return true;
     }
+
+    public static Aeroporto Buscar(String nome)
+    {
+        for(Aeroporto x : vetaero)
+        {
+            if(nome.equals(x.getNome()))
+            {
+                return x;
+            }
+        }
+        return null;
+    }
+
+
+    public static boolean Existe(String nome)
+    {
+        for(Aeroporto x : vetaero)
+        {
+            if(nome.equals(x.getNome()))
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static boolean Remove(String nome)
+    {
+        for(Aeroporto x : vetaero)
+        {
+            if(nome.equals(x.getNome()))
+            {
+                vetaero.remove(x);
+                return true;
+            }
+        }
+        return false;
+    }
+
 }
